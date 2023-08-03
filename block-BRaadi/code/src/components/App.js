c5e15be834b74ec90122cc56b301a244d0c945cf
Ravi kumar <rkravikr9102@gmail.com>
2023-08-03 22:35:46 +0530

    Done Assignment: Error Boundary

--- block-BRaadi/code/src/components/App.js
+++ block-BRaadi/code/src/components/App.js
@@ -1,14 +1,17 @@
-import React from "react";
-import Header from "./Header";
-import Main from "./Main";
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import ErrorBoundary from '../ErrorBoundary';
 
 class App extends React.Component {
   render() {
     let { isDarkMode } = this.state;
     return (
       <>
-        <Header isDarkMode={isDarkMode} />
-        <Main isDarkMode={isDarkMode} />
+        <ErrorBoundary fallback={<p>Something went wrong</p>}>
+          <Header isDarkMode={isDarkMode} />
+          <Main isDarkMode={isDarkMode} />
+        </ErrorBoundary>
       </>
     );
   }
--- block-BRaadi/code/src/components/Header.js
+++ block-BRaadi/code/src/components/Header.js
@@ -1,11 +1,16 @@
+import React from "react";
+import ErrorBoundary from "../ErrorBoundary";
+
 function Header() {
   return (
+    <ErrorBoundary>
     <header>
       <div className="header-content">
         <h1>ERROR BOUNDARY</h1>
         <p>Make sure to test error boundary in production build!</p>
       </div>
     </header>
+    </ErrorBoundary>
   );
 }
 
